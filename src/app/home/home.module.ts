360e58cd87a7b6d4d6dc31435bcdd51040508a57
LucianaMerege <lucianama@dcc.ufrj.br>
2020-01-30 15:49:03 -0300

    trabalho typescript

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { PostComponent } from '../component/post/post.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, PostComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,91 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  username:string;
+  curtiu:boolean;
+  descurtiu:boolean;
+  numCurtidas:number;
+  numDescurtidas:number;
+  texto:string;
+  spoiler:boolean;
+  
+  dadosPost:object[] = [
+    {
+      username: "chaosIsALadder",
+      curtiu: false,
+      descurtiu: false,
+      numCurtidas: 29,
+      numDescurtidas:77,
+      texto: "Eles morrem no episódio 12",
+      spoiler: true
+    },
+    {
+      username: "teddybear",
+      curtiu: false,
+      descurtiu: false,
+      numCurtidas: 105,
+      numDescurtidas:44,
+      texto: "Mano, amei o ep 11, q bom q acabou td bem",
+      spoiler: false 
+    },
+    {
+      username: "user2000",
+      curtiu: false,
+      descurtiu: false,
+      numCurtidas: 33,
+      numDescurtidas:12,
+      texto: "Melhor série aaaaaaaaa",
+      spoiler: false
+    }
+  ]
+  
+    curtida (dadosPost) {
+      if (dadosPost['curtiu'] == false){
+        dadosPost['numCurtidas']++;
+      }
+      else{
+        dadosPost['numCurtidas']--;
+      }
+      if (dadosPost.descurtiu == true)
+      {
+        dadosPost.descurtiu= !dadosPost.descurtiu;
+        dadosPost.numDescurtidas--;
+      }
+      dadosPost.curtiu= !dadosPost.curtiu;
+    }
+    descurtida (dadosPost) {
+      if (dadosPost['descurtiu'] == false){
+        dadosPost['numDescurtidas']++;
+      }
+      else{
+        dadosPost['numDescurtidas']--;
+      }
+      if (dadosPost.curtiu == true)
+      {
+        dadosPost.curtiu= !dadosPost.curtiu;
+        dadosPost.numCurtidas--;
+      }
+      dadosPost.descurtiu= !dadosPost.descurtiu;
+    }
+
+  async alerta() {
+    const alert = await this.alertController.create({
+      header: 'Irado!',
+      message: 'Gostaria de avaliar o filme?',
+      buttons: ['Não', 'Sim']
+    });
+    await alert.present();
+  }
+  
+  async presentToast(){
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado na sua lista!',
+      duration: 2000
+    });
+    toast.present();
+  }
+
 
 }
